37ad3a3a1ccdb4746af7a030dd635debb13e8a0e
Prerak Mathur <prerakmathur@Preraks-MacBook-Pro-2.local>
2023-01-24 08:29:02 -0800

    homescreen done

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,23 @@
-import { StyleSheet } from 'react-native';
+import * as React from  'react';
+import { Text,View, Image, StyleSheet, FlatList } from 'react-native';
+import ChatRoomComponent from '../components/ChatRoomComponent'; 
+import ChatRoomData from '../assets/dummy-data/ChatRooms';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
-
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+    <View style = {styles.page}>
+  <FlatList 
+  data = {ChatRoomData}
+  renderItem = { ({item}) => <ChatRoomComponent chatRoom={item} />} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+  page:{
+//just the div for each chat componet on tab1
+backgroundColor:'white',
+flex:1,  
+},
+  
 });
